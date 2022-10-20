8143c202f0dd13010db09e9287cba7d7c243e79f
Mauricio Acosta <bernardoz209@icloud.com>
2022-10-20 11:38:54 -0700

    Fixed Verification

--- Program.cs
+++ Program.cs
@@ -1,26 +1,48 @@
 ﻿//Mauricio Acosta
 //10-18-22
 //AddTwoNumbers
-
+string numberOne = "";
+string numberTwo = "";
+int numOne;
+int numTwo;
 string playAgain = "yes";
-
+bool isConverted = false;
+bool isConverted2 = false;
 while (playAgain == "yes")
 {
     Console.Clear();
     Console.WriteLine("Lets add two numbers");
-Console.WriteLine("Enter your first number");
-string numberOne = Console.ReadLine();
+    while (isConverted == false)
+    {
+        Console.WriteLine("Enter your first number");
+        numberOne = Console.ReadLine();
+        isConverted = Int32.TryParse(numberOne, out numOne);
+        if (isConverted == false)
+        {
+            Console.WriteLine("Enter a valid Number");
+        }
+    }
 
-Console.WriteLine("Enter the second number");
-string numberTwo = Console.ReadLine();
-
-int num1 = Convert.ToInt32(numberOne);
-int num2 = Convert.ToInt32(numberTwo);
 
 
-Console.WriteLine("Your sum is:" + (num1 + num2));
 
-Console.WriteLine("Would you like to play again, type yes to play again");
 
-playAgain = Console.ReadLine();
+    while (isConverted2 == false)
+    {
+        Console.WriteLine("Enter the second number");
+        numberTwo = Console.ReadLine();
+        isConverted2 = Int32.TryParse(numberTwo, out numTwo);
+        if (isConverted2 == false)
+        {
+            Console.WriteLine("Enter a valid Number");
+        }
+    }
+
+
+
+    Console.WriteLine("Your sum is:" + (numberOne + numberTwo));
+
+    Console.WriteLine("Would you like to play again, type yes to play again");
+
+    playAgain = Console.ReadLine();
 }
